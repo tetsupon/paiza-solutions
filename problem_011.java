b1e8562aa1346661b110a64b20102d5cf425779f
tetsupon <te.ka.20230305@gmail.com>
2024-09-10 18:54:00 +0900

    Add solustion for problem 011

---
+++ problem_011.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String message = scan.next();
+        System.out.println(message);
+    }
+}
