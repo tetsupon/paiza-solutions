aa385fab03a8a2d5583331f0a1453cb01920a88a
tetsupon <te.ka.20230305@gmail.com>
2024-09-11 07:08:50 +0900

    Add solustion for problem 013

---
+++ problem_013.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();  //整数の場合はint numberは分かりやすいように、整数を受け取る場合はnextIntメソッドを使//
+        System.out.println(number);
+    }
+}
