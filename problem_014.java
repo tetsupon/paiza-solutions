1edf23b8b6b596e0de6ca52b680ce3b57eae5559
tetsupon <te.ka.20230305@gmail.com>
2024-09-11 14:24:35 +0900

    Add solustion for problem 014

---
+++ problem_014.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        System.out.println("リンゴが" + number + "個");
+    }
+}
