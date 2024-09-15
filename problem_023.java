1f7d2dc2ae172c52237ce7e0ccaff63c69830010
tetsupon <te.ka.20230305@gmail.com>
2024-09-15 18:40:41 +0900

    Add solustion for problem 023

---
+++ problem_023.java
@@ -0,0 +1,10 @@
+//同じ処理を何度も繰り返す（演習）
+
+public class Main {
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 100; i++){           //カウンタ変数（int i = 0;）でi++（変数iを1増やす）を100より小さい場合(99まで)まで繰り返し処理をする。（0〜99まで）
+            System.out.println("Hello paiza");
+        }
+    }
+}
