b55712758cb1d0a68b4568c67784978e3652b52d
tetsupon <te.ka.20230305@gmail.com>
2024-09-18 06:57:41 +0900

    Add solustion for problem 028 in Java

---
+++ java/problem_028.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String message = scan.next();
+        System.out.println(message);
+        String text = scan.next();
+        System.out.println(text);
+    }
+}
