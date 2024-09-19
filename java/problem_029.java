25c175d1fa041f30b348560670e976c3ccd1f159
tetsupon <te.ka.20230305@gmail.com>
2024-09-19 19:56:08 +0900

    Add solustion for problem 029 in Java

---
+++ java/problem_029.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        
+        if (number >= 100){
+            System.out.println(number);
+        }else {
+            System.out.println();
+        }
+    }
+}
