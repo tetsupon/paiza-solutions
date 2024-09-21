25bcf0cefc99ba6f645614d512589d986bd568dd
tetsupon <te.ka.20230305@gmail.com>
2024-09-22 07:20:16 +0900

    Add solution for problwm 015 in Java Java体験

---
+++ java/Java入門/problem_015.java
@@ -0,0 +1,8 @@
+// 変数を使う（演習３）
+
+public class Main {
+	public static void main(String[] args) {
+		String player = "勇者と戦士";                      //変数名teamをplayerに変更
+		System.out.println(player + "の体力が回復した");
+	}
+}
