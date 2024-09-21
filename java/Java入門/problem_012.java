c052179d4137f0684044539564d56996fbd2e986
tetsupon <te.ka.20230305@gmail.com>
2024-09-22 07:10:32 +0900

    Add solution for problwm 012 in Java Java体験

---
+++ java/Java入門/problem_012.java
@@ -0,0 +1,10 @@
+// 変数を使う
+
+public class Main {
+	public static void main(String[] args) {
+	    String player = "勇者";                                //変数名をplayerにして"勇者"という文字列を代入
+		System.out.println(player + "、荒野を歩いていた");         //変数名 + 文字列で表示させることができる
+		System.out.println(player + "、モンスターと戦った");
+		System.out.println(player + "、モンスターをたおした");
+	}
+}
