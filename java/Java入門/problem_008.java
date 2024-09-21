f4f4bbc78b146d2ee56c3348920313908337145e
tetsupon <te.ka.20230305@gmail.com>
2024-09-21 12:05:34 +0900

    Add solution for problwm 008 in Java Java体験

---
+++ java/Java入門/problem_008.java
@@ -0,0 +1,11 @@
+//コメントでプログラムを見やすく（演習２）
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("勇者は、荒野を歩いていた");
+		//System.out.println("A:モンスターがあらわれた");
+		System.out.println("勇者は、すごい剣を手にいれた");
+		//System.out.println("A:ドラゴンがあらわれた");
+		//System.out.println("A:魔王があらわれた");
+		System.out.println("勇者は、世界を救った");
+	}
