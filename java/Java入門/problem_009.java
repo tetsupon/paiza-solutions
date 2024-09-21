487d5c143ca3926eebad604872fad04317ad0f2a
tetsupon <te.ka.20230305@gmail.com>
2024-09-21 12:14:00 +0900

    Add solution for problwm 009 in Java Java体験

---
+++ java/Java入門/problem_009.java
@@ -0,0 +1,9 @@
+// HTMLを表示する
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("<h1>hello world</h1>");    //改行したくない場合は、lnを削除。（HTMLで表示する場合は関係ない）（textで表示する場合のみ影響する）
+		System.out.println("<p>世界の皆さん、");
+	    System.out.println("<b>こんにちは</b></p>");
+	}
+}
