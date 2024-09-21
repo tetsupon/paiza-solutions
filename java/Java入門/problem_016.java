1ab8e8f1392fd35b36f86331a049099104e9bb92
tetsupon <te.ka.20230305@gmail.com>
2024-09-22 07:22:05 +0900

    Add solution for problwm 016 in Java Java体験

---
+++ java/Java入門/problem_016.java
@@ -0,0 +1,10 @@
+// 変数を使う（演習４）
+
+public class Main {
+	public static void main(String[] args) {
+		String player = "勇者";
+		System.out.println(player + "は、荒野を歩いていた");
+		System.out.println(player +"は、モンスターと戦った");     //player +を追加
+		System.out.println(player +"は、モンスターをたおした");   //player +を追加
+	}
+}
