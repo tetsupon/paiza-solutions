321da729c5c37f004c7b99348e6e8368d1b2190b
tetsupon <te.ka.20230305@gmail.com>
2024-09-22 07:18:13 +0900

    Add solution for problwm 014 in Java Java体験

---
+++ java/Java入門/problem_014.java
@@ -0,0 +1,8 @@
+// 変数を使う（演習２）
+
+public class Main {
+	public static void main(String[] args) {
+		String player = "勇者";                          //player1をplayerに変更
+		System.out.println(player + "の体力が回復した");
+	}
+}
