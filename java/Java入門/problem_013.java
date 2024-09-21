430af7fa9603fb479fb2562e616e9d0efe0c861c
tetsupon <te.ka.20230305@gmail.com>
2024-09-22 07:13:14 +0900

    Add solution for problwm 012 in Java Java体験

---
+++ java/Java入門/problem_013.java
@@ -0,0 +1,8 @@
+// 変数を使う（演習１）
+
+public class Main {
+	public static void main(String[] args) {
+		String player = "勇者";
+		System.out.println(player + "は、レベルアップした");    //変数player + "文字列"で出力
+	}
+}
