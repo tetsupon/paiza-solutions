e37329d184962e1b9d748766d916b09fdb8ef8a6
tetsupon <te.ka.20230305@gmail.com>
2024-09-23 17:00:58 +0900

    Add solution for problwm 029 in Java Java体験

---
+++ java/Java入門/problem_029.java
@@ -0,0 +1,9 @@
+// データの種類（演習３）
+
+public class Main {
+	public static void main(String[] args) {
+		String a = "01234";                       //文字列の表示は Stringと""が必要
+		String b = "56789";
+		System.out.println(a + b);
+	}
+}
