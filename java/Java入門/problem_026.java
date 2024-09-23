289d66c6618e2e0318a2a29f035e282db1a84e6f
tetsupon <te.ka.20230305@gmail.com>
2024-09-23 16:52:21 +0900

    Add solution for problwm 026 in Java Java体験

---
+++ java/Java入門/problem_026.java
@@ -0,0 +1,12 @@
+// データの種類
+
+public class Main {
+	public static void main(String[] args) {
+		int number = 100 + 30;	// 数値                 数値の場合は int
+		String text = "paiza" + "paiza";	// 文字列     文字列の場合はString
+		System.out.println(number);
+		System.out.println(text);
+		System.out.println(number + text);
+		System.out.println(number + 20);
+	}
+}
