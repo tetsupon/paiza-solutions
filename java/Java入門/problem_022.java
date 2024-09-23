2d9f158b9afc2a219fef9afc8bfbc2ca5d1bf83d
tetsupon <te.ka.20230305@gmail.com>
2024-09-23 16:15:56 +0900

    Add solution for problwm 022 in Java Java体験

---
+++ java/Java入門/problem_022.java
@@ -0,0 +1,11 @@
+//計算する（演習３）
+
+public class Main {
+	public static void main(String[] args) {
+		int x = 8;
+		int y = 5;
+		// 以下に、xとyを割ったときの余りを計算し、結果を出力するコードを書いてください
+	    int number = x % y ;     //変数(x) % 変数(y)で余りを表示
+		System.out.println(number);
+	}
+}
