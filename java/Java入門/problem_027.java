3d8a155f1f62d66c56e0fdf9ead442cd0ba0ea7d
tetsupon <te.ka.20230305@gmail.com>
2024-09-23 16:55:54 +0900

    Add solution for problwm 027 in Java Java体験

---
+++ java/Java入門/problem_027.java
@@ -0,0 +1,8 @@
+// データの種類
+
+public class Main {
+	public static void main(String[] args) {
+		int x = 50;                               //"50"の文字列になっているのを 50の数値に変更
+		System.out.println(x - 10);
+	}
+}
