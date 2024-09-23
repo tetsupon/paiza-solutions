a473607e9b968c5ee75c7994d5d62c2fba467ab7
tetsupon <te.ka.20230305@gmail.com>
2024-09-23 18:16:49 +0900

    Add solution for problwm 001 in Java Java入門2

---
+++ java/Java入門2/problem_001.java
@@ -0,0 +1,13 @@
+// if文による条件分岐
+
+public class Main {
+	public static void main(String[] args) {
+	    int number = 2;
+	    if(number == 1){
+	         System.out.println("スキ！"); //条件式が成立した時の処理
+	    }else {
+	        System.out.println("キライ");//条件式が成立しなかった時の処理
+	        System.out.println("キライ");
+	    }
+	}
+}
