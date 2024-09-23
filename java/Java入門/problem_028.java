75e7cd6144f8520ac9fa9d4f941844d4f3f21614
tetsupon <te.ka.20230305@gmail.com>
2024-09-23 16:58:14 +0900

    Add solution for problwm 028 in Java Java体験

---
+++ java/Java入門/problem_028.java
@@ -0,0 +1,9 @@
+// データの種類（演習２）
+
+public class Main {
+	public static void main(String[] args) {
+		String a = "モンスターが";                 //文字列にあｈ""を付ける
+		String b = "あらわれた";
+		System.out.println(a + b);
+	}
+}
