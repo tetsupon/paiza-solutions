18854872429dd049c7be8476ff476206d6327f58
tetsupon <te.ka.20230305@gmail.com>
2024-09-23 16:13:16 +0900

    Add solution for problwm 021 in Java Java体験

---
+++ java/Java入門/problem_021.java
@@ -0,0 +1,12 @@
+//計算する（演習２）
+
+public class Main {
+	public static void main(String[] args) {
+		int a = 31;
+		int b = 17;
+		// 以下に、aとbのかけ算し、結果を出力するコードを書いてください
+	    int number = a * b ;   //変数（a） * 変数(b)
+		System.out.println(number);
+
+	}
+}
