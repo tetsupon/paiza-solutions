455318afb4362bad44397e3f68368dcc9f46539e
tetsupon <te.ka.20230305@gmail.com>
2024-10-01 17:42:00 +0900

    Add solution for problwm 025 in Java Java入門2

---
+++ java/Java入門2/problem_025.java
@@ -0,0 +1,8 @@
+// 文字列を数値に変換しよう（演習１）
+
+public class Main {
+    public static void main(String[] args) {
+        String text = "0.08";
+        System.out.println(Double.parseDouble(text));
+    }
+}
