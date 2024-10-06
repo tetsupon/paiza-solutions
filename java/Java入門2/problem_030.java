f1e1498e4d29b9ef49be06c0cf3cc04540500b76
tetsupon <te.ka.20230305@gmail.com>
2024-10-06 12:47:26 +0900

    Add solution for problwm 030 in Java Java入門2

---
+++ java/Java入門2/problem_030.java
@@ -0,0 +1,9 @@
+// whileによるループ処理（演習２）
+public class Main {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i <= 15) {
+            System.out.println(i);
+            i = i + 1;
+        }
+    }
