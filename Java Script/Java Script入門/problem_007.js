096f50a1340926dcf19c448478f945665ad3feea
tetsupon <te.ka.20230305@gmail.com>
2024-10-07 20:02:01 +0900

    Add solution for problwm 007 in Java Java Script入門

---
+++ Java Script/Java Script入門/problem_007.js
@@ -0,0 +1,8 @@
+// 変数を使う
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var player = "戦士"
+
+console.log(player + "は、荒野を歩いていた");
+console.log(player + "は、モンスターと戦った");
+console.log(player + "は、モンスターをたおした");
