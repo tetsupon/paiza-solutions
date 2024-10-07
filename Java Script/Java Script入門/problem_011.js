c469a1799e45e03ac6d9ce7a5024652564cb4505
tetsupon <te.ka.20230305@gmail.com>
2024-10-08 07:40:16 +0900

    Add solution for problwm 011 in Java Java Script入門

---
+++ Java Script/Java Script入門/problem_011.js
@@ -0,0 +1,8 @@
+// 変数を使う(演習４)
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var player = "勇者";
+
+console.log(player + "は、荒野を歩いていた");
+console.log(player + "は、モンスターと戦った");
+console.log(player + "は、モンスターをたおした");
