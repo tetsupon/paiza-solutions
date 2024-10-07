f4b2279881337c1d6e3872084a6e876e4734909f
tetsupon <te.ka.20230305@gmail.com>
2024-10-07 19:41:03 +0900

    Add solution for problwm 003 in Java Java Script入門

---
+++ Java Script/Java Script入門/problem_003.js
@@ -0,0 +1,8 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+console.log("勇者は、荒野を歩いていた");
+//console.log("A:モンスターがあらわれた");
+console.log("勇者は、すごい剣を手にいれた");
+//console.log("A:ドラゴンがあらわれた");
+//console.log("A:魔王があらわれた");
+console.log("勇者は、世界を救った");
