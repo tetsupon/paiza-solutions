80dcd7500a5647a02f3e1f1a2e2b88a4c5443a88
tetsupon <te.ka.20230305@gmail.com>
2024-10-11 19:56:40 +0900

    Add solution for problwm 021 in Java Java Script入門

---
+++ Java Script/Java Script入門/problem_021.js
@@ -0,0 +1,9 @@
+// データの種類
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var number = 100 + 40;// 数値
+var text = "ハロー" + "paiza";// 文字列
+console.log(number);
+console.log(text);
+console.log(number + text);
+console.log(number + 20);
