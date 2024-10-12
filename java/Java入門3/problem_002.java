a9f2942f1a51c93909b8e02226a657521dfcb2b2
tetsupon <te.ka.20230305@gmail.com>
2024-10-12 10:12:19 +0900

    Add solution for problwm 002 in Java Java入門3

---
+++ java/Java入門3/problem_002.java
@@ -0,0 +1,11 @@
+// whileによるループ処理（演習１）
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i <= 9) {
+            System.out.println("ハローpaizaラーニング");
+            i = i + 1;
+        }
+    }
+}    
