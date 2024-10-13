6c7d5d40d98c21af0bb6f9c1e889171bfeb5f791
tetsupon <te.ka.20230305@gmail.com>
2024-10-13 18:31:02 +0900

    Add solution for problwm 013 in Java Java入門3

---
+++ java/Java入門3/problem_013.java
@@ -0,0 +1,9 @@
+// forによるループ処理(演習３)
+public class Main {
+    public static void main(String[] args) {
+        int i;
+        for(i = 1; i <= 12; i++){
+            System.out.println(i + "月");
+        }
+    }
+}
