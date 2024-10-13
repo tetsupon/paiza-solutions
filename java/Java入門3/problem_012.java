64688acdddd400337c505b69720c02b0c2fe4f51
tetsupon <te.ka.20230305@gmail.com>
2024-10-13 18:27:48 +0900

    Add solution for problwm 012 in Java Java入門3

---
+++ java/Java入門3/problem_012.java
@@ -0,0 +1,9 @@
+// forによるループ処理(演習２)
+public class Main {
+    public static void main(String[] args) {
+        int i;
+        for(i = 0; i <=15; i++){
+            System.out.println(i);
+        }
+    }
+}
