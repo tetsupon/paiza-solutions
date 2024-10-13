fb6f88eeabff9d14d8091594554f1a39b114640a
tetsupon <te.ka.20230305@gmail.com>
2024-10-13 18:24:38 +0900

    Add solution for problwm 011 in Java Java入門3

---
+++ java/Java入門3/problem_011.java
@@ -0,0 +1,8 @@
+// forによるループ処(演習１)
+public class Main {
+    public static void main(String[] args) {
+        int i;
+        for(i = 0; i <= 9; i++){                        
+            System.out.println("ハローpaizaラーニング");
+        }
+    }
