6c781ae319fd6b286ac5a19d500a4055cef42536
tetsupon <te.ka.20230305@gmail.com>
2024-10-14 15:48:07 +0900

    Add solution for problwm 017 in Java Java入門3

---
+++ java/Java入門3/problem_017.java
@@ -0,0 +1,10 @@
+// 標準入力(演習１)
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       String line = sc.next();
+       System.out.println(line);
+    }
+}
