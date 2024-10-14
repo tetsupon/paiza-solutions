a0fc67cbc5498317eb3458d9bd5355c57bb5da7a
tetsupon <te.ka.20230305@gmail.com>
2024-10-14 15:53:35 +0900

    Add solution for problwm 018 in Java Java入門3

---
+++ java/Java入門3/problem_018.java
@@ -0,0 +1,10 @@
+// 標準入力(演習２)
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int line = sc.nextInt();                 //整数型(int)にする
+        System.out.println(line * 100);
+    }
+}
