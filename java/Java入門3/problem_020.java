07a6d700b0282b9179e2276f77a95bae8fd2a59d
tetsupon <te.ka.20230305@gmail.com>
2024-10-16 12:27:19 +0900

    Add solution for problwm 020 in Java Java入門3

---
+++ java/Java入門3/problem_020.java
@@ -0,0 +1,12 @@
+// 標準入力とループ処理(演習１)
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        for(int i = 0; i < count; i++) {
+            System.out.println("スライムがあらわれた");
+        }
+    }
+}
