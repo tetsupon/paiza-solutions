45234037692c7604037ce9688537584a1502ece9
tetsupon <te.ka.20230305@gmail.com>
2024-10-18 08:51:45 +0900

    Add solution for problwm 003 in Java Java入門4

---
+++ java/Java入門4/problem_003.java
@@ -0,0 +1,10 @@
+// 指定の文字を配列にする(演習２)
+
+public class Main {
+    public static void main(String[] args) {
+        String item[] = {"ロングソード", "ブレードソード", "エクスカリバー"};
+        System.out.println(item[0]);
+        System.out.println(item[1]);
+        System.out.println(item[2]);
+    }
+}
