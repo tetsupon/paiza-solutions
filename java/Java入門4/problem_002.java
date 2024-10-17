1e39ec03b321eeecebdc5e0dc23e1be20afec48a
tetsupon <te.ka.20230305@gmail.com>
2024-10-18 08:49:39 +0900

    Add solution for problwm 002 in Java Java入門4

---
+++ java/Java入門4/problem_002.java
@@ -0,0 +1,11 @@
+// 配列の中身を出力してみよう(演習１)
+public class Main {
+    public static void main(String[] args) {
+        String[] array = {"戦士","侍","僧侶","魔法使い"};
+        // この下で、arrayを出力してみよう
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+    }
+}
