308ca3373c626492ad62b215fb532bf50bcdd74b
tetsupon <te.ka.20230305@gmail.com>
2024-10-22 08:32:57 +0900

    Add solution for problwm 007 in Java Java入門4

---
+++ java/Java入門4/problem_007.java
@@ -0,0 +1,9 @@
+// 配列から特定要素を取り出す(演習１)
+
+public class Main {
+    public static void main(String[] args) {
+        String[] team = {"勇者", "戦士", "侍", "忍者", "魔法使い"};
+        // teamの1番左の要素をprintlnメソッドで出力する
+        System.out.println(team[0]); 
+    }
+}
