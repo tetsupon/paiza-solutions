92c0520ed7a21d2cbd404050e89de13b51a555b0
tetsupon <te.ka.20230305@gmail.com>
2024-10-23 19:11:22 +0900

    Add solution for problwm 013 in Java Java入門4

---
+++ java/Java入門4/problem_013.java
@@ -0,0 +1,11 @@
+// 配列の中身をループで表示する(演習１)
+
+public class Main {
+    public static void main(String[] args) {
+        String[] enemy = {"スライム", "モンスター", "ゾンビ", "ドラゴン", "魔王"};
+        // ここに、要素をループで表示するコードを記述する
+        for (String i : enemy){
+            System.out.println(i + "が現れた");
+        }
+    }
+}
