521c0d7717068c5a5949a09328b2b1553831fb57
tetsupon <te.ka.20230305@gmail.com>
2024-10-28 17:52:04 +0900

    Add solution for problwm 003 in Java Java入門5

---
+++ java/Java入門5/problem_003.java
@@ -0,0 +1,16 @@
+// 配列の中身を出力してみよう(演習２)
+public class Main {
+    public static void main(String[] args) {
+        String[][] array = {{"勇者", "忍者"}, {"武士", "戦士"}, {"僧侶", "魔法使い"}};
+
+        // この下で、arrayの全ての要素を出力してみよう
+        System.out.println(array[0][0]);
+        System.out.println(array[0][1]);
+        System.out.println(array[1][0]);
+        System.out.println(array[1][1]);
+        System.out.println(array[2][0]);
+        System.out.println(array[2][1]);
+
+
+    }
+}
