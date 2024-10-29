05b81badc54dbca966dd187fd6572d12d67b39df
tetsupon <te.ka.20230305@gmail.com>
2024-10-29 09:46:15 +0900

    Add solution for problwm 006 in Java Java入門5

---
+++ java/Java入門5/problem_006.java
@@ -0,0 +1,9 @@
+// 2次元配列の要素の個数を出力する(演習２)
+public class Main {
+    public static void main(String[] args) {
+        String[][] basket = {{"木の棒", "こん棒"}, {"おにぎり", "おにぎり"}, {"毒消し", "薬草"}};
+
+        // ここに、個数を出力するコードを記述する
+        System.out.println(basket[1].length);    //basket配列のインデックス要素[1]の個数をlengthで
+    }
+}
